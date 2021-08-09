9272f3b26965c7da5142084b840926e31fac94d1
chidokun <chidokun@users.noreply.github.com>
2021-08-09 06:38:56 +0000

    deploy: 5794c5d62787738dc3c7e34233a0948553fff9f1

--- js/main.js
+++ js/main.js
@@ -5,23 +5,28 @@ var katexOpts = { delimiters:[
     { left: "$"   , right: "$"   , display: false },
     { left: "\\(" , right: "\\)" , display: false }
   ]};
-  // render KaTex Math
-  renderMathInElement(document.body, katexOpts);
-  
-  // Language Select
-  $('#language').on('change', function() {
-    window.location.pathname = window.location.pathname.replace(/\/(.*?)\//i, '/'+ this.value +'/');
-  });
-  
-  // Editor
-  $('#editor textarea').on('input change', function() {
-    var preview = document.getElementById('preview-content');
-    var rawTxt = this.value.trim();
-    if (rawTxt.length < 1) {
-      return preview.innerHTML = rawTxt;
-    }
-    // to markdown
-    preview.innerHTML = marked(rawTxt);
-    // to KaTeX Math
-    renderMathInElement(preview, katexOpts);
-  });
+// render KaTex Math
+renderMathInElement(document.body, katexOpts);
+
+// Language Select
+$('#language').on('change', function() {
+  window.location.pathname = window.location.pathname.replace(/\/(.*?)\//i, '/'+ this.value +'/');
+});
+
+// Editor
+$('#editor textarea').on('input change', function() {
+  var preview = document.getElementById('preview-content');
+  var rawTxt = this.value.trim();
+  if (rawTxt.length < 1) {
+    return preview.innerHTML = rawTxt;
+  }
+  // to markdown
+  preview.innerHTML = marked(rawTxt);
+  // to KaTeX Math
+  renderMathInElement(preview, katexOpts);
+});
+
+function changeValue(elementName, newValue){
+  document.getElementsByName(elementName)[0].value=newValue;
+  window.location.hash = "#comment-form";
+};
